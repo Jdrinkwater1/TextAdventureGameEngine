d479ac5a6a23fd2c4e3d02555d11397894ac308d
Mrhyperman <Mrhyperman@jacobspc>
2020-03-06 19:22:38 -0500

    Start of new GUI

---
+++ src/GameGUI3.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+import java.awt.*;
+import javax.swing.*;
+/**
+ * @author Jacob Drinkwater
+ *
+ */
+public class GameGUI3 {
+
+	private JButton helpButton,
+					lookButton,
+					northButton,
+					eastButton,
+					southButton,
+					westButton,
+					inButton,
+					takeButton,
+					dropButton;
+					
+	private JTextArea gameText;
+	private JTextField inputText;
+	
+	private JLabel gameLabel;
+					
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
